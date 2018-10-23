82003f075ce3c2e9412b1fef2efff2b791a9df7b
Rachel Daniel <rachdaniel88@gmail.com>
2018-10-23 13:36:25 -0500

    completed advanced form validation challenge

--- scripts/journal.js
+++ scripts/journal.js
@@ -22,19 +22,32 @@ document.addEventListener("submit", (e) => {
 
   const acceptableChars = [ " ", ".", ",", "!", "'", "?", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "(", ")", "{", "}", ":", ";", "\""]
 
-  const entryChars = concepts.value + journalEntry.value + dailyMood.value
+  const entryChars = `${concepts.value} ${journalEntry.value} ${dailyMood.value}`
+  const entryNoPunct = entryChars.replace(/[.,\/!\^&\*;:{}()?]/g,"").toLowerCase()
+  const entryWords = entryNoPunct.split(" ")
   let correctChars = true
+  let noSwearing = true
 
+  //loop through all characters entered and check against acceptable characters
   for (i = 0; i < entryChars.length; i++) {
     if (acceptableChars.indexOf(entryChars[i].toLowerCase()) === -1) {
       correctChars = false
     } 
   }
 
+  //loop through all entered words (minus punctuation) and check for swear words
+  for (i = 0; i < entryWords.length; i++) {
+    if (entryWords[i] === "fuck" || entryWords[i] === "shit" || entryWords[i] === "damn" ||entryWords[i] === "ass" || entryWords[i] === "bitch" || entryWords[i] === "hell" || entryWords[i] === "dammit" || entryWords[i] === "godammit" ) {
+      noSwearing = false
+    } 
+  }
+  
   if (dailyMood.value === "select") {
     alert("Please select a current mood before submitting")
   } else if (correctChars === false) {
     alert("Please do not use non-alphabetic, non-numeric characters other than basic punctuation, (), {}, : or ;")
+  } else if (noSwearing === false) {
+    alert("Please don't swear-- it hurts my feelings.")
   } else {
    //create an object with the input values
     let currentEntry = {
